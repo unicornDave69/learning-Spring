d28e1192ac0d6a61962a68d2b12afa9278026628
David Dobrovolny <david.dobrovolny@plus4u.net>
2025-02-25 17:46:43 +0100

    creating class example Student

---
+++ src/main/java/com/david/example/Student.java
@@ -0,0 +1,66 @@
+package com.david.example;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String lastname;
+    private String email;
+    private Integer age;
+
+    public Student(String name, String surname, String lastname, String email, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.lastname = lastname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
